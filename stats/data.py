b806421da2a6830c1b3d78f28611f2227c5d2a96
Ryan Roby <rroby@aota.org>
2019-10-01 15:23:28 -0400

    fix

--- stats/data.py
+++ stats/data.py
@@ -1,14 +1,14 @@
 import os
 import glob
-import panda as pd
+import pandas as pd
 
-game_files = glob.glob(os.path.join(os.getcwd(), 'games','*eve'))
+game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*eve'))
 game_files.sort()
 
 game_frames = []
 for game_file in game_files:
-    game_frame = pd.read_csv(game_file, names=['type', 'multi2','multi3',
-    'multi4', 'multi5', 'multi6','event'])
+    game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3',
+                                               'multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
 
 games = pd.concat(game_frames)
@@ -19,7 +19,7 @@ identifiers = games['multi5'].str.extract(r'(.LS9\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
 identifiers.columns = ['game_id', 'year']
 
-games = pd.concat([games, identifiers], axis=1,sort=false)
+games = pd.concat([games, identifiers], axis=1, sort='false')
 
 games = games.fillna(' ')
 
